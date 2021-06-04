1e098588114b1bbc181ff6299a95ab85df3a7103
Omar Irfan <omar.irfan18@gmail.com>
2021-06-04 16:14:51 +0000

    added AI controlled paddle

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,6 +1,10 @@
 import Phaser from 'phaser'
 
 export default class Game extends Phaser.Scene {
+
+  init(){
+    this.paddleRightVelocity = new Phaser.Math.Vector2(0,0)
+  }
   
   preload(){
 
@@ -8,20 +12,25 @@ export default class Game extends Phaser.Scene {
 
   create(){
 
-    const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
-    this.physics.add.existing(ball)
+    this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+    this.physics.add.existing(this.ball)
 
-    ball.body.setBounce(1,1)
+    this.ball.body.setBounce(1,1)
 
-    ball.body.setCollideWorldBounds(true,1,1)
+    this.ball.body.setCollideWorldBounds(true,1,1)
 
-    ball.body.setVelocity(-200,0)
+    this.ball.body.setVelocity(Phaser.Math.Between(-200, 200),Phaser.Math.Between(-200, 200))
 
     this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
+
+    this.paddleRight = this.add.rectangle(750, 250, 30, 100, 0xffffff, 1)
+
+    this.physics.add.existing(this.paddleRight, true)
     
     this.physics.add.existing(this.paddleLeft, true)
 
-    this.physics.add.collider(this.paddleLeft, ball)
+    this.physics.add.collider(this.paddleLeft, this.ball)
+    this.physics.add.collider(this.paddleRight, this.ball)
 
     this.cursors = this.input.keyboard.createCursorKeys()
 
@@ -40,6 +49,31 @@ export default class Game extends Phaser.Scene {
       body.updateFromGameObject()
     }
 
+    const diff = this.ball.y - this.paddleRight.y
+
+    if (Math.abs(diff) < 10) {
+      return
+    }
+
+    const aiSpeed = 1
+    
+    if (diff < 0) {
+      this.paddleRightVelocity.y = -aiSpeed
+      if (this.paddleRightVelocity.y < -10) {
+        this.paddleRightVelocity.y= -10
+      }
+    }
+    
+    else if (diff > 0) {
+      this.paddleRightVelocity.y = aiSpeed
+      if (this.paddleRightVelocity.y > 10) {
+        this.paddleRightVelocity.y= 10
+      }
+    }
+
+    this.paddleRight.y += this.paddleRightVelocity.y
+    this.paddleRight.body.updateFromGameObject
+    
   }
 
 }
